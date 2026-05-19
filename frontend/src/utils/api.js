c12c3ea2bea38c8acd960edac6c2420c7bc24eef
ilyadyn <egortocenov@gmail.com>
2026-05-19 19:54:18 +0300

    Обновлен API URL на Render

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://water-calorie-tracker.onrender.com'
+    baseURL: 'https://water-calorie-tracker.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
